04d655e85c7466bcc052140d8c238c83cfb625b7
chmamai <marco.mailand@ch.abb.com>
2018-05-17 16:42:52 +0200

    some changes according to book.

--- csv-reader.py
+++ csv-reader.py
@@ -1,5 +1,28 @@
 import csv
-with open('C:\Users\chmamai\Documents\logging-example\my.csv', 'r+', newline='') as csv_file:
-	reader = csv.reader(csv_file)
-	for row in reader:
-		print(str(row))
+
+def read(file_location):
+    with open(file_location, 'r+', newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        return [row for row in reader]
+def write(file_location, rows):
+    with open(file_location, 'w+', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        for row in rows:
+            writer.writerow(row)
+
+def raw_test():
+    columns = int(input("How many columns do you want to write? "))
+    input_rows = []
+    keep_going = True
+    while keep_going:
+        input_rows.append([input("column {}: ".format(i + 1)) for i in range(0, columns)])
+        ui_keep_going = input("continue? (y/N): ")
+        if ui_keep_going != "y":
+            keep_going = False
+
+    print(str(input_rows))
+    write('raw.csv', input_rows)
+    written_value = read('raw.csv')
+    print(str(written_value))
+
+raw_test()
