456a33aa2b4b5ae383f54ad38bd338b6862737a7
Isaac2k1 <newton2k1@gmail.com>
2018-08-07 18:17:01 +0200

    finding parameters A and B

--- curveplot1.py
+++ curveplot1.py
@@ -19,9 +19,9 @@ select case iCurve
  Case 1 sValues = array("0"    , pi/3600*3575, pi/360000*56285 , -0.010378 , -94.79208 , "A"    ,  1000 , 10001,  0.0, 2E-5)
  Case 2 sValues = array("1000" , pi/3600*3575, pi/360000*56285 , -0.010378 , -94.79208 , "A"    ,  0    , 10001,  0.0, 2E-5)
 """
-A = pi/3600*3575
-B = pi/360000*56285
-C = 1000
+A = pi/180000*95000
+B = pi/180000*80000
+C = 1 #000
 pp1 = -0.010378
 pp2 = -94.79208
 O = 0
@@ -32,20 +32,25 @@ sample = 20000
 x = np.arange(sample)
 #y = np.sin(2 * np.pi * f * x / Fs)
 
-sON     =  abs((0>np.sin(100*pi*x/Fs+A)) * (0>np.sin(100*pi*x/Fs+B)) )
-sOFF    =  abs((0<np.sin(100*pi*x/Fs+A)) * (0>np.sin(100*pi*x/Fs+B)) )
+sON     =  abs((0<np.sin(200*pi*x/Fs+A)) )#* (0<np.sin(200*pi*x/Fs+B)) )
+sOFF    =  abs((0>np.sin(200*pi*x/Fs+B)) )#* (0>np.sin(200*pi*x/Fs+B)) )
 sSINrec =  abs(np.sin(100*pi* x/Fs ))
 sDECH   =  pp1* (np.arctan(np.tan(100*pi* x/Fs )) + pp2)
 
 # sFormula1 = "C*(" & sON  & "*" & sSINrec & "+" & sOFF & " * " & sDECH & ") + O"        
-y           =  sON
-#y1          =  sOFF
-#y            =  C*                  sSINrec + O
-#y1           =  C*(    sON     *    sSINrec    +    sOFF     *     sDECH    ) + O
-#y1          =  C*  sSINrec + O
+y           =  sON * sOFF #sSINrec
+y1          =  sON + 1.1
+y2          =  sOFF + 2.2
+y3          =  C*                  sSINrec                                   + O -1.1
+y4          =  C*                                                 sDECH      + O
+y5          =  C*(    sON     *    sSINrec    +    sOFF     *     sDECH    ) + O
 
-
-plt.plot(x, y) #, y1)
+plt.plot(x, y)
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+#plt.plot(x, y4)
+#plt.plot(x, y5)
 plt.xlabel('sample(n)')
 plt.ylabel('voltage(V)')
 plt.show()
