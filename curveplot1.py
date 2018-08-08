3bc76c8bfaf0abfa66b9f702f8449ba96f107c01
Isaac2k1 <newton2k1@gmail.com>
2018-08-08 17:40:36 +0200

    curves now correct
    The initial values for A and B from VBS code could be used in slightly modified version, haha. The other two values, pp1 and pp2 can be used w/o any change, since they are amplitudes. Now the VBS code should be adapted to reflect the Python code.

--- curveplot1.py
+++ curveplot1.py
@@ -19,38 +19,31 @@ select case iCurve
  Case 1 sValues = array("0"    , pi/3600*3575, pi/360000*56285 , -0.010378 , -94.79208 , "A"    ,  1000 , 10001,  0.0, 2E-5)
  Case 2 sValues = array("1000" , pi/3600*3575, pi/360000*56285 , -0.010378 , -94.79208 , "A"    ,  0    , 10001,  0.0, 2E-5)
 """
-A = pi/180000*95000
-B = pi/180000*80000
-C = 1 #000
+A = pi/360000*416285
+B = pi/360000*357500
+C = 1000
 pp1 = -0.010378
 pp2 = -94.79208
 O = 0
 
-Fs = 1000000
-f = 100
-sample = 20000
-x = np.arange(sample)
-#y = np.sin(2 * np.pi * f * x / Fs)
+Fs      = 1000000
+f       = 100
+sample  = 20000
+x       = np.arange(sample)
 
-sON     =  abs((0<np.sin(200*pi*x/Fs+A)) )#* (0<np.sin(200*pi*x/Fs+B)) )
-sOFF    =  abs((0>np.sin(200*pi*x/Fs+B)) )#* (0>np.sin(200*pi*x/Fs+B)) )
-sSINrec =  abs(np.sin(100*pi* x/Fs ))
-sDECH   =  pp1* (np.arctan(np.tan(100*pi* x/Fs )) + pp2)
+sON     = abs((0<np.sin(200*pi*x/Fs+A)) * (0>np.sin(200*pi*x/Fs+B)) )
+sOFF    = 1 - sON 
+sSINrec = abs(np.sin(100*pi* x/Fs ))
+sDECH   = pp1* (np.arctan(np.tan(100*pi* x/Fs )) + pp2)
 
 # sFormula1 = "C*(" & sON  & "*" & sSINrec & "+" & sOFF & " * " & sDECH & ") + O"        
-y           =  sON * sOFF #sSINrec
-y1          =  sON + 1.1
-y2          =  sOFF + 2.2
-y3          =  C*                  sSINrec                                   + O -1.1
-y4          =  C*                                                 sDECH      + O
-y5          =  C*(    sON     *    sSINrec    +    sOFF     *     sDECH    ) + O
+y1      = C *   sSINrec                             + O 
+y2      = C *                   sDECH               + O 
+y3      = C * ( sSINrec * sON + sDECH * (1 - sON )) + O 
 
-plt.plot(x, y)
-plt.plot(x, y1)
-plt.plot(x, y2)
-plt.plot(x, y3)
-#plt.plot(x, y4)
-#plt.plot(x, y5)
+plt.plot(x, y1, color='b')
+plt.plot(x, y2, color='g')
+plt.plot(x, y3, color='r')
 plt.xlabel('sample(n)')
 plt.ylabel('voltage(V)')
 plt.show()
