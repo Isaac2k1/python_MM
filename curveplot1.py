41ef2a6d334a5f021bacf82c47d1f27230ca575b
Isaac2k1 <newton2k1@gmail.com>
2019-09-02 00:39:31 +0200

    three colours DCI-A2 curve

--- curveplot1.py
+++ curveplot1.py
@@ -1,49 +1,10 @@
-<<<<<<< HEAD
-import matplotlib.pyplot as plt
-import numpy as np
-from  math import*
-
-"""
-Sub setDCI(iCurve,sSymbols,sValues,sUnit,Pts,t0,dt)
-'      sSymbols = array("Ampl" , "Start"     ,  "Stop"         , "pp1"     , "pp2"     , "sUnit", "O"   , "Pts", "t0", "dt")
-       sSymbols = array("C"    , "A"         ,  "B"            , "pp1"     , "pp2"     , "sUnit", "O"   , "Pts", "t0", "dt")
-select case iCurve
- Case 1 sValues = array("0"    , pi/3600*3575, pi/360000*56285 , -0.010378 , -94.79208 , "A"    ,  1000 , 10001,  0.0, 2E-5)
- Case 2 sValues = array("1000" , pi/3600*3575, pi/360000*56285 , -0.010378 , -94.79208 , "A"    ,  0    , 10001,  0.0, 2E-5)
-"""
-A = pi/3600*3575
-B = pi/360000*56285
-C = 1000
-pp1 = -0.010378
-pp2 = -94.79208
-O = 0
-
-Fs = 1000000
-f = 100
-sample = 20000
-x = np.arange(sample)
-#y = np.sin(2 * np.pi * f * x / Fs)
-
-sON     =  abs((0>np.sin(200*pi*x/Fs+A)) + (0>np.sin(200*pi*x/Fs+B))  )
-sOFF    =  abs((0<=np.sin(200*pi* x/Fs +A )) + (0<=np.sin(200*pi* x/Fs +B )))
-sSINrec =  abs(np.sin(100*pi* x/Fs ))
-sDECH   =  pp1* (np.arctan(np.tan(100*pi* x/Fs )) + pp2)
-
-               
-y = C *( sON * sSINrec + sOFF * sDECH ) + O
-
-plt.plot(x, y)
-plt.xlabel('sample(n)')
-plt.ylabel('voltage(V)')
-plt.show()
-=======
+#runs in Python 3.7.4 on MacOS
 # curveplot1.py
 import matplotlib.pyplot as plt
 import numpy as np
 from  math import*
 
 """
-
 sON     =  "abs(   (0>sin(200*pi*Ch(""t"")+A )) AND   (0>sin(200*pi*Ch(""t"")+B )))"
 sOFF    =  "abs(NOT(0>sin(200*pi*Ch(""t"")+A )) OR NOT(0>sin(200*pi*Ch(""t"")+B )))"
 sSINrec =  "abs(sin(100*pi*Ch(""t"")))"
@@ -88,4 +49,3 @@ plt.plot(t, y3, color='r')
 plt.xlabel('sample(n)')
 plt.ylabel('voltage(V)')
 plt.show()
->>>>>>> 300d99f32a143a936fd9dcfc8b472349d83ec1bf
