1c28ad55cad8073e4d737aef5b45cf2d717b0b39
Marco Mailand <newton2k1@gmail.com>
2019-10-02 14:04:27 +0200

    set has benn introduced

--- hangwoman.py
+++ hangwoman.py
@@ -1,4 +1,30 @@
 searchword="hangthewhitch"
-input("give me letter:",inputchar)
-print("input was:", inputchar)
-# so lan
+print('Your word has %d letters.' % len(searchword))
+print(len(searchword))
+foundword = '-' * len(searchword)
+foundletters = set()
+
+for i in searchword:    
+    print('-', end='')
+print(' ')
+print (searchword)
+
+
+lives = 6
+while True:
+    print('\nYou have {0:d} trials.'.format(lives))
+    inputchar = input("Enter a letter:")
+    print("input was:", inputchar)
+    lives -= 1
+    for i in searchword:
+        if inputchar in foundletters:
+            print(inputchar, end='')
+            lives += 1
+            if inputchar == i:
+                print(inputchar, end='')
+                
+            else:
+                print('-', end='')
+            
+    if lives <= 0:
+        break
