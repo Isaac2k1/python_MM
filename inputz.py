ef08686e7824565df3a85c99edfef295343052fb
Isaac2k1 <newton2k1@gmail.com>
2019-11-05 19:25:53 +0100

    initial commit, test of input of strings

---
+++ inputz.py
@@ -0,0 +1,6 @@
+name    = input("Enter Employee Name")
+salary  = input("Enter salary")
+company = input ("Enter Company name")
+print("Printing Employee Details")
+print ("Name", "Salary", "Company")
+print (name, salary, company)
