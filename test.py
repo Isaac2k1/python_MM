71016200f32b0c2de6366cafab0d3d545a1c3cf7
Isaac2k1 <newton2k1@gmail.com>
2020-03-02 20:39:25 +0100

    Python 3 adaptations

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-ui_keep_going = raw_input("continue? (y/N): ")
-print ui_keep_going
+import sys
+print(sys.platform)
+print(2**100)
+#raw_input()  # this has renamed in Python3 to input
+input()
+ui_keep_going = input("continue? (y/N): ")
+print(ui_keep_going)
