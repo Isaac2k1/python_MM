aafa484f20d72d100e2cbd7e688e39b7b7450b30
Isaac2k1 <newton2k1@gmail.com>
2020-04-27 09:16:01 +0200

    many Jupyter notebooks added
    WorkingCopy is not a very easy to use iPad app. I cheated a lot to get
    the Synchronisation of an iPad folder working.

--- quersum.py
+++
@@ -1,6 +0,0 @@
-inp=input("Enter a number to calculate queersum: ")
-qsum=0
-for i in inp:
-    qsum += int(i)
-    print('digit=%s int.sum=%d' % (i,qsum))  #debug command
-print('The queersum of %s is %d' % (inp,qsum))
