1e938c1be9b04a7efff43400d273133104fda52d
Isaac2k1 <newton2k1@gmail.com>
2021-01-02 21:07:53 +0100

    started to implement Snake-code from HPI

--- schildi.py
+++ schildi.py
@@ -1,5 +1,16 @@
 from turtle import *
-forward(100)
-right(120)
-forward(100)
-input()
+
+kopf = Turtle()
+kopf.shape("square")
+kopf.color("black")
+kopf.penup()
+kopf.speed(0)
+kopf.goto(0, 0)
+kopf.direction = "stop"
+
+essen = Turtle()
+essen.shape("circle")
+essen.color("red")
+essen.penup()
+essen.speed(0)
+essen.goto(0, 100)
