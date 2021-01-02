ef13b004c14ad16fc9af003220a4ed30ed4b9cde
Isaac2k1 <newton2k1@gmail.com>
2021-01-02 21:12:59 +0100

    started to implement Snake-code from HPI, 1/3

--- schildi.py
+++ schildi.py
@@ -14,3 +14,48 @@ essen.color("red")
 essen.penup()
 essen.speed(0)
 essen.goto(0, 100)
+
+rechts = Turtle()
+rechts.shape("triangle")
+rechts.color("green")
+rechts.penup()
+rechts.speed(0)
+rechts.goto(180, -160)
+
+unten = Turtle()
+unten.shape("triangle")
+unten.color("green")
+unten.right(90)
+unten.penup()
+unten.speed(0)
+unten.goto(160, -180)
+
+links = Turtle()
+links.shape("triangle")
+links.color("green")
+links.right(180)
+links.penup()
+links.speed(0)
+links.goto(140, -160)
+
+oben = Turtle()
+oben.shape("triangle")
+oben.color("green")
+oben.right(270)
+oben.penup()
+oben.speed(0)
+oben.goto(160, -160)
+
+def interpretiere_eingabe(x, y):
+    if (x >=  150 and x <=  170
+    and y >= -190 and y <= -170):
+        nach_unten_ausrichten()
+    elif (x >=  170 and x <=  190)
+    and   y >= -170 and y <= -150):
+        nach_rechts_ausrichten()
+    elif (x >=  170 and x <=  190)
+    and   y >= -170 and y <= -150):
+        nach_oben_ausrichten()
+    elif (x >=  170 and x <=  190)
+    and   y >= -170 and y <= -150):
+        nach_links_ausrichten()
