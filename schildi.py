ab828de49df0af3675aa7ee5a3705e69183e7537
Isaac2k1 <newton2k1@gmail.com>
2021-01-04 12:20:55 +0100

    shape not working in Pythonista

--- schildi.py
+++ schildi.py
@@ -1,7 +1,7 @@
 from turtle import *
 
 kopf = Turtle()
-kopf.shape("square")
+#kopf.shape("square")
 kopf.color("black")
 kopf.penup()
 kopf.speed(0)
@@ -9,21 +9,21 @@ kopf.goto(0, 0)
 kopf.direction = "stop"
 
 essen = Turtle()
-essen.shape("circle")
+#essen.shape("circle")
 essen.color("red")
 essen.penup()
 essen.speed(0)
 essen.goto(0, 100)
 
 rechts = Turtle()
-rechts.shape("triangle")
+#rechts.shape("triangle")
 rechts.color("green")
 rechts.penup()
 rechts.speed(0)
 rechts.goto(180, -160)
 
 unten = Turtle()
-unten.shape("triangle")
+#unten.shape("triangle")
 unten.color("green")
 unten.right(90)
 unten.penup()
@@ -31,7 +31,7 @@ unten.speed(0)
 unten.goto(160, -180)
 
 links = Turtle()
-links.shape("triangle")
+#links.shape("triangle")
 links.color("green")
 links.right(180)
 links.penup()
@@ -39,12 +39,12 @@ links.speed(0)
 links.goto(140, -160)
 
 oben = Turtle()
-oben.shape("triangle")
+#oben.shape("triangle")
 oben.color("green")
 oben.right(270)
 oben.penup()
 oben.speed(0)
-oben.goto(160, -160)
+oben.goto(160, -140)
 
 def interpretiere_eingabe(x, y):
     if   (x >=  150 and x <=  170) and (y >= -190 and y <= -170):
@@ -53,5 +53,7 @@ def interpretiere_eingabe(x, y):
         nach_rechts_ausrichten()
     elif (x >=  170 and x <=  190) and (y >= -170 and y <= -150):
         nach_oben_ausrichten()
-    elif (x >=  170 and x <=  190) and (y >= -170 and y <= -150):
+    elif (x >=  170 and x <=  190) and (y >= -150 and y <= -130):
         nach_links_ausrichten()
+        
+
