a84c3f8f0084f144edcc62de6187f981992ad67e
Isaac2k1 <newton2k1@gmail.com>
2021-01-04 01:33:24 +0100

    coordinates improved for cursors

--- schildi.py
+++ schildi.py
@@ -47,11 +47,11 @@ oben.speed(0)
 oben.goto(160, -160)
 
 def interpretiere_eingabe(x, y):
-    if   (x >=  150 and x <=  170) and (y >= -190 and y <= -170):
+    if   (x >=  150 and x <=  170 and y >= -190 and y <= -170):
         nach_unten_ausrichten()
-    elif (x >=  170 and x <=  190) and (y >= -170 and y <= -150):
+    elif (x >=  170 and x <=  190 and y >= -170 and y <= -150):
         nach_rechts_ausrichten()
-    elif (x >=  170 and x <=  190) and (y >= -170 and y <= -150):
+    elif (x >=  150 and x <=  170 and y >= -170 and y <= -150):
         nach_oben_ausrichten()
-    elif (x >=  170 and x <=  190) and (y >= -170 and y <= -150):
+    elif (x >=  130 and x <=  150 and y >= -150 and y <= -130):
         nach_links_ausrichten()
