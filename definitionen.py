f63e4a9e1b2cb4abc7923d75ce068abf3da17dd0
Isaac2k1 <newton2k1@gmail.com>
2021-01-11 23:52:51 +0100

    code does not really work

--- definitionen.py
+++ definitionen.py
@@ -1,6 +1,67 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 # Definitionen für Snake-Spiel
+import turtle
+segmente = []
+kopf = turtle.Turtle()
+kopf.shape("square")
+kopf.color("black")
+kopf.shape("square")
+kopf.penup()
+kopf.speed(0)
+kopf.goto(0, 0)
+kopf.direction = "stop"
+
+essen = turtle.Turtle()
+essen.shape("circle")
+essen.color("red")
+essen.penup()
+essen.speed(0)
+essen.goto(0, 100)
+
+rechts = turtle.Turtle()
+rechts.shape("triangle")
+rechts.color("green")
+rechts.penup()
+rechts.speed(0)
+rechts.goto(180, -160)
+
+unten = turtle.Turtle()
+unten.shape("triangle")
+unten.color("green")
+unten.right(90)
+unten.penup()
+unten.speed(0)
+unten.goto(160, -180)
+
+links = turtle.Turtle()
+links.shape("triangle")
+links.color("green")
+links.right(180)
+links.penup()
+links.speed(0)
+links.goto(140, -160)
+
+oben = turtle.Turtle()
+oben.shape("triangle")
+oben.color("green")
+oben.right(270)
+oben.penup()
+oben.speed(0)
+oben.goto(160, -140)
+
+neues_segment = turtle.Turtle()
+neues_segment.shape("square")
+neues_segment.color("yellow")
+neues_segment.penup()
+segmente.append(neues_segment)
+
+def wiederhole_spiellogik():
+    checke_kollision_mit_essen()
+    checke_kollision_mit_fensterrand()
+    koerper_bewegen()
+    kopf_bewegen()
+    checke_kollision_mit_segmenten()
 
 def interpretiere_eingabe(x, y):
     print(x, y)
@@ -12,7 +73,7 @@ def interpretiere_eingabe(x, y):
         nach_oben_ausrichten()
     elif (x >=  170 and x <=  190) and (y >= -150 and y <= -130):
         nach_links_ausrichten()
-        
+
 
     elif (x >=  130 and x <=  150 and y >= -170 and y <= -150):
         nach_links_ausrichten()
@@ -53,13 +114,18 @@ def kopf_bewegen():
         kopf.setx(x - 20)
 
 def checke_kollision_mit_essen():
-    if kopf.distance(essen) < 20:
+    if kopf.distance(essen) < 15:
         # essen an neue Position kopf_bewegen
         # Schlange wachsen lassen
-        hugo = hugo
+        x = random.randint(-180, 180)
+        y = random.randint(-180, 180)
+        food.goto(x, y)
 
 def checke_kollision_mit_fensterrand():
     if kopf.xcor() < -190 or kopf.xcor() > 190 or kopf.ycor() < -190 or kopf.ycor() > 190:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
         spiel_neustarten()
         # Kopf in der Mitte platzieren
         # Richtung auf "stop" setzen
@@ -69,11 +135,29 @@ def checke_kollision_mit_fensterrand():
 def checke_kollision_mit_segmenten():
     for segment in segmente:
         if segment.distance(kopf) < 20:
-            spiel_neustarten()
+            time.sleep(1)
+            kopf.goto(0, 0)
+            kopf.direction = "stop"
+            segmente_entfernen()
 
 def koerper_bewegen():
     for index in range(len(segmente) - 1, 0, -1):
         # bewege segmente(index) an segmente(index - 1)
-        i = 1
+        x = segmente[index-1].xcor()
+        y = segmente[index-1].ycor()
+        segmente[index].goto(x, y)
+        if len(segmente) < 0:
+            x = head.xcor()
+            y = head.ycor()
+            segmente[0].goto(x, y)
     # überprüfe, ob die Schlange nicht nur aus Kopf besteht
         # Wenn, dann bewege erstes Segment zum Kopf
+
+def segmente_entfernen():
+# Hide the segments
+    for segment in segmente:
+        segment.goto(1000, 1000)
+        # clear segment list
+        segmente = []
+
+#def spiel_neustarten():
--- schildi.py
+++ schildi.py
@@ -1,70 +1,10 @@
-from turtle import *
 from definitionen import *
-from random import *
+import random
+#from random import *
 
-kopf = Turtle()
-#kopf.shape("square")
-kopf.color("black")
-kopf.shape("square")
-kopf.penup()
-kopf.speed(0)
-kopf.goto(0, 0)
-kopf.direction = "stop"
-
-essen = Turtle()
-#essen.shape("circle")
-essen.color("red")
-essen.penup()
-essen.speed(0)
-essen.goto(0, 100)
-
-rechts = Turtle()
-#rechts.shape("triangle")
-rechts.color("green")
-rechts.penup()
-rechts.speed(0)
-rechts.goto(180, -160)
-
-unten = Turtle()
-#unten.shape("triangle")
-unten.color("green")
-unten.right(90)
-unten.penup()
-unten.speed(0)
-unten.goto(160, -180)
-
-links = Turtle()
-#links.shape("triangle")
-links.color("green")
-links.right(180)
-links.penup()
-links.speed(0)
-links.goto(140, -160)
-
-oben = Turtle()
-#oben.shape("triangle")
-oben.color("green")
-oben.right(270)
-oben.penup()
-oben.speed(0)
-oben.goto(160, -140)
-
-neues_segment = Turtle()
-neues_segment.shape("square")
-neues_segment.color("yellow")
-neues_segment.penup()
-neues_segment.speed(0)
-neues_segment.goto(170, -170)
-
-segmente = []
-
-onclick(interpretiere_eingabe)
-
-
-def wiederhole_spiellogik():
-    checke_kollision_mit_essen()
-    checke_kollision_mit_fensterrand()
-    koerper_bewegen()
-    kopf_bewegen()
-    checke_kollision_mit_segmenten()
-
+# Main game loop
+print("START")
+while True:
+    turtle.onclick(interpretiere_eingabe)
+    wiederhole_spiellogik()
+print("STOP")
