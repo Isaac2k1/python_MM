c5a4428f71cafdd717e9fa9efdae34b29e76094a
Marco_Mailand <marco.mailand@hitachienergy.com>
2022-05-03 18:11:46 +0200

    ReEx started working

--- WordSearch/W5.py
+++ WordSearch/W5.py
@@ -1,4 +1,4 @@
-# W5 is a WORDLE helper to find five letter words and search for them
+﻿# W5 is a WORDLE helper to find five letter words and search for them
 # Ale's refactor from 2020-03-29 19h01
 import itertools
 import re
@@ -17,7 +17,9 @@ def analineW5(line, liste):
     return liste
 
 # === PREPARE WORDLIST ===
-ObjRead = open("web2.txt", "r")
+path = "C:\\Users\\chmamai\\OneDrive - Hitachi Energy\\Documents\\Python Scripts\\python_MM\\WordSearch\\"
+filepath = path + "web2.txt"
+ObjRead = open(filepath, "r")
 print("start reading wordlist...")
 words5 = 0
 totalwords = 0
@@ -32,31 +34,31 @@ with ObjRead as textfile:
 
 liste = list(liste)
 liste.sort()
+print('found words:', len(liste))
 
+# https://docs.python.org/3/library/re.html
 found = []
-ReEx = re.compile(r'OO')
+ReEx = re.compile('[SQ]')
 
 #find all the words starting with 'a' or 'e'
 for word in liste:
-    found.append(ReEx.search(word))
+    if ReEx.search(word):
+        found.append(word)
 
-print('found words:', len(found), found)
-
-print("List had", totalwords, "words, found",len(found),"five letter words")
 ObjRead.close()
 
 column = 0
 lines = 0
 nofcol = 10
 
-for word in liste:
+for word in found:
     print(word, end=' ')
     column += 1
     if not(column % nofcol):
         print(lines)
         lines += 1
 
-print("\nList had", totalwords, "words, found") #,len(liste),"five letter words")
+print("\nList had", totalwords, "words, found",len(found),"five letter words")
 
 # Enter up to 5 letters to get suggestions for WORDLE.
 
